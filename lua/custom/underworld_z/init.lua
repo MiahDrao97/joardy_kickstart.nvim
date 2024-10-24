0ad4d3758aa5aee6314b2325c130f4043b7808ee
j_oard_pfl <joard@pfl.com>
2024-10-24 16:43:09 -0600

    altered the colors again

--- lua/custom/underworld_z/init.lua
+++ lua/custom/underworld_z/init.lua
@@ -1,69 +1,94 @@
 local M = {}
 
+local colors = {
+  ctrl_flow_pink = '#ec4a90',
+  dark_blue = '#3070b4',
+  eyesore_pink = '#e15ac1',
+  cmd_mode_orange = '#cfaf60',
+  violet = '#cb9ffa',
+  macro_orange = '#c28f58',
+  namespace_white = '#cdcdcd',
+  bracket_grey = '#6a7899',
+  operator_white = '#b9b9b9',
+  static_periwinkle = '#66f2ff',
+  prop_blue_grey = '#84a8cf',
+  param_blue = '#4aa9f0',
+  err_tag_maroon = '#b4206a',
+  method_orange = '#e0cf90',
+  interface_orange = '#eab57a',
+  string_rusty_orange = '#c68f78',
+  string_escape_yellow = '#ffe890',
+  variable_pink = '#d491bd',
+  struct_class_green = '#5ad1b3',
+  struct_green = '#70ca70',
+  comment_grey = '#565f89',
+}
+
 M.descend_to_the_underworld = function()
   -- we're giving treesitter the edge here
   vim.highlight.priorities.treesitter = 126
 
-  vim.api.nvim_set_hl(0, '@boolean', { fg = '#3070b4' })
-  vim.api.nvim_set_hl(0, '@keyword', { fg = '#3070b4' })
-  vim.api.nvim_set_hl(0, '@constant.builtin', { fg = '#3070b4' })
-  vim.api.nvim_set_hl(0, '@type.builtin', { fg = '#3070b4' })
-  vim.api.nvim_set_hl(0, '@variable.builtin', { fg = '#3070b4' })
-  vim.api.nvim_set_hl(0, '@keyword.operator', { fg = '#3070b4' })
-  vim.api.nvim_set_hl(0, '@keyword.function', { fg = '#3070b4', force = true })
-  vim.api.nvim_set_hl(0, '@keyword.exception', { fg = '#ec4a90', force = true })
-  vim.api.nvim_set_hl(0, '@keyword.conditional', { fg = '#ec4a90', force = true })
-  vim.api.nvim_set_hl(0, '@keyword.return', { fg = '#ec4a90', force = true })
-  vim.api.nvim_set_hl(0, '@keyword.repeat', { fg = '#ec4a90', force = true })
-  vim.api.nvim_set_hl(0, '@lsp.type.typeParameter', { fg = '#ec4a90' })
+  vim.api.nvim_set_hl(0, '@boolean', { link = '@keyword' })
+  vim.api.nvim_set_hl(0, '@keyword', { fg = colors.dark_blue })
+  vim.api.nvim_set_hl(0, '@constant.builtin', { link = '@keyword' })
+  vim.api.nvim_set_hl(0, '@type.builtin', { fg = colors.violet })
+  vim.api.nvim_set_hl(0, '@variable.builtin', { link = '@keyword' })
+  vim.api.nvim_set_hl(0, '@keyword.operator', { link = '@keyword' })
+  vim.api.nvim_set_hl(0, '@keyword.function', { link = '@keyword' })
+  vim.api.nvim_set_hl(0, '@keyword.exception', { fg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, '@keyword.conditional', { fg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, '@keyword.return', { fg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, '@keyword.repeat', { fg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, '@lsp.type.typeParameter', { fg = colors.ctrl_flow_pink })
   vim.api.nvim_set_hl(0, 'underworld_z.typeparam', { link = '@lsp.type.typeParameter' })
-  vim.api.nvim_set_hl(0, '@lsp.type.type', { fg = '#e15ac1' })
-  vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#ec4a90' })
-  vim.api.nvim_set_hl(0, 'IncSearch', { fg = '#000000', bg = '#ec4a90' })
-  vim.api.nvim_set_hl(0, 'MiniStatuslineModeInsert', { fg = '#000000', bg = '#ec4a90' })
-  vim.api.nvim_set_hl(0, 'MiniStatuslineModeCommand', { fg = '#000000', bg = '#cfaf60' })
-  vim.api.nvim_set_hl(0, 'MiniStatuslineModeVisual', { fg = '#000000', bg = '#cb9af7' })
-  vim.api.nvim_set_hl(0, '@function.builtin', { fg = '#af7b48' })
-  vim.api.nvim_set_hl(0, '@keyword.import', { fg = '#af7b48' })
+  vim.api.nvim_set_hl(0, '@lsp.type.type', { fg = colors.violet })
+  vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, 'IncSearch', { fg = '#000000', bg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, 'MiniStatuslineModeInsert', { fg = '#000000', bg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, 'MiniStatuslineModeCommand', { fg = '#000000', bg = colors.cmd_mode_orange })
+  vim.api.nvim_set_hl(0, 'MiniStatuslineModeVisual', { fg = '#000000', bg = colors.violet })
+  vim.api.nvim_set_hl(0, '@function.builtin', { fg = colors.macro_orange })
+  vim.api.nvim_set_hl(0, '@keyword.import', { fg = colors.macro_orange })
   vim.api.nvim_set_hl(0, '@keyword.import.c_sharp', { link = '@keyword' }) -- link to keyword for c# (just 'using' kw)
-  vim.api.nvim_set_hl(0, '@keyword.directive', { fg = '#af7b48' })
-  vim.api.nvim_set_hl(0, '@lsp.type.builtin.zig', { fg = '#af7b48' })
-  vim.api.nvim_set_hl(0, '@module', { fg = '#cdcdcd' })
+  vim.api.nvim_set_hl(0, '@keyword.directive', { fg = colors.macro_orange })
+  vim.api.nvim_set_hl(0, '@lsp.type.builtin.zig', { fg = colors.macro_orange })
+  vim.api.nvim_set_hl(0, '@module', { fg = colors.namespace_white })
   vim.api.nvim_set_hl(0, 'underworld_z.namespace', { link = '@module' })
   vim.api.nvim_set_hl(0, '@punctuation.delimiter', { link = '@operator' })
-  vim.api.nvim_set_hl(0, '@punctuation.bracket', { fg = '#707f99' })
-  vim.api.nvim_set_hl(0, '@punctuation.special', { fg = '#76eaff' })
-  vim.api.nvim_set_hl(0, '@variable.parameter', { fg = '#cb9af7' })
-  vim.api.nvim_set_hl(0, 'underworld_z.parameter', { fg = '#cb9af7' })
-  vim.api.nvim_set_hl(0, '@lsp.type.enumMember', { fg = '#4fc1ff' })
-  vim.api.nvim_set_hl(0, '@lsp.type.property', { fg = '#4fc1ff' })
-  vim.api.nvim_set_hl(0, '@variable.member', { fg = '#4fc1ff' })
-  vim.api.nvim_set_hl(0, '@property', { fg = '#4fc1ff' })
-  vim.api.nvim_set_hl(0, 'underworld_z.property', { fg = '#4fc1ff' })
-  vim.api.nvim_set_hl(0, '@lsp.type.errorTag', { fg = '#b4206a' })
-  vim.api.nvim_set_hl(0, '@lsp.type.function', { fg = '#76eaff' })
-  vim.api.nvim_set_hl(0, 'underworld_z.static_method', { fg = '#76eaff' })
-  vim.api.nvim_set_hl(0, '@lsp.type.method', { fg = '#e0cf90' })
-  vim.api.nvim_set_hl(0, '@function.method', { fg = '#e0cf90' })
-  vim.api.nvim_set_hl(0, '@function.call', { fg = '#e0cf90' })
-  vim.api.nvim_set_hl(0, '@lsp.type.string', { fg = '#c68f78' })
-  vim.api.nvim_set_hl(0, '@lsp.type.number', { fg = '#ec4a90' })
-  vim.api.nvim_set_hl(0, '@number', { fg = '#ec4a90' })
-  vim.api.nvim_set_hl(0, '@lsp.type.variable', { fg = '#d491bd' })
-  vim.api.nvim_set_hl(0, '@variable', { fg = '#d491bd' })
-  vim.api.nvim_set_hl(0, '@lsp.type.interface', { fg = '#eab57a' })
+  vim.api.nvim_set_hl(0, '@punctuation.bracket', { fg = colors.bracket_grey })
+  vim.api.nvim_set_hl(0, '@punctuation.special', { fg = colors.static_periwinkle })
+  vim.api.nvim_set_hl(0, '@variable.parameter', { fg = colors.param_blue })
+  vim.api.nvim_set_hl(0, 'underworld_z.parameter', { link = '@variable.parameter' })
+  vim.api.nvim_set_hl(0, '@lsp.type.enumMember', { fg = colors.static_periwinkle })
+  vim.api.nvim_set_hl(0, '@lsp.type.property', { link = '@property' })
+  vim.api.nvim_set_hl(0, '@variable.member', { link = '@property' })
+  vim.api.nvim_set_hl(0, '@property', { fg = colors.prop_blue_grey })
+  vim.api.nvim_set_hl(0, 'underworld_z.property', { link = '@property' })
+  vim.api.nvim_set_hl(0, '@lsp.type.errorTag', { fg = colors.err_tag_maroon })
+  vim.api.nvim_set_hl(0, '@lsp.type.function', { fg = colors.static_periwinkle })
+  vim.api.nvim_set_hl(0, 'underworld_z.static', { fg = colors.static_periwinkle })
+  vim.api.nvim_set_hl(0, '@lsp.type.method', { link = '@function.method' })
+  vim.api.nvim_set_hl(0, '@function.method', { fg = colors.method_orange })
+  vim.api.nvim_set_hl(0, '@function.call', { fg = colors.method_orange })
+  vim.api.nvim_set_hl(0, '@lsp.type.string', { link = '@string' })
+  vim.api.nvim_set_hl(0, '@lsp.type.number', { link = '@number' })
+  vim.api.nvim_set_hl(0, '@number', { fg = colors.ctrl_flow_pink })
+  vim.api.nvim_set_hl(0, '@lsp.type.variable', { link = '@variable' })
+  vim.api.nvim_set_hl(0, '@variable', { fg = colors.variable_pink })
+  vim.api.nvim_set_hl(0, '@lsp.type.interface', { fg = colors.interface_orange })
   vim.api.nvim_set_hl(0, 'underworld_z.interface', { link = '@lsp.type.interface' })
-  vim.api.nvim_set_hl(0, '@string', { fg = '#c68f78' })
-  vim.api.nvim_set_hl(0, '@string.escape', { fg = '#ffe890' })
-  vim.api.nvim_set_hl(0, '@character', { fg = '#c68f78' })
-  vim.api.nvim_set_hl(0, 'Type', { fg = '#5ad1b3' })
+  vim.api.nvim_set_hl(0, '@string', { fg = colors.string_rusty_orange })
+  vim.api.nvim_set_hl(0, '@string.escape', { fg = colors.string_escape_yellow })
+  vim.api.nvim_set_hl(0, '@character', { link = '@string' })
+  vim.api.nvim_set_hl(0, 'Type', { fg = colors.struct_class_green })
   vim.api.nvim_set_hl(0, 'underworld_z.class', { link = 'Type' })
-  vim.api.nvim_set_hl(0, 'underworld_z.struct', { fg = '#70ca70' })
-  vim.api.nvim_set_hl(0, 'underworld_z.enum', { fg = '#e15ac1' })
-  vim.api.nvim_set_hl(0, 'underworld_z.enumMember', { fg = '#cdcdcd' })
-  vim.api.nvim_set_hl(0, '@attribute', { fg = '#5ad1b3' })
-  vim.api.nvim_set_hl(0, '@constructor', { fg = '#5ad1b3' })
-  vim.api.nvim_set_hl(0, '@operator', { fg = '#b9b9b9' })
+  vim.api.nvim_set_hl(0, 'underworld_z.struct', { fg = colors.struct_green })
+  vim.api.nvim_set_hl(0, 'underworld_z.enum', { fg = colors.violet })
+  vim.api.nvim_set_hl(0, 'underworld_z.enumMember', { fg = colors.static_periwinkle })
+  vim.api.nvim_set_hl(0, '@attribute', { link = 'Type' })
+  vim.api.nvim_set_hl(0, '@constructor', { link = 'Type' })
+  vim.api.nvim_set_hl(0, '@operator', { fg = colors.operator_white })
+  vim.api.nvim_set_hl(0, '@constant', { fg = colors.param_blue })
 
   -- background
   vim.api.nvim_set_hl(0, 'Normal', { bg = '#1a001a' })
@@ -93,13 +118,18 @@ M.descend_to_the_underworld = function()
           priority = 128, -- this puts it right at the top
         })
       end
-      if token.type == 'property' then
+      if token.type == 'property' and not token.modifiers.static then
         vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, 'underworld_z.property', {
           priority = 128, -- this puts it right at the top
         })
       end
       if token.type == 'method' and token.modifiers.static then
-        vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, 'underworld_z.static_method', {
+        vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, 'underworld_z.static', {
+          priority = 128, -- this puts it right at the top
+        })
+      end
+      if token.type == 'property' and token.modifiers.static then
+        vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, 'underworld_z.static', {
           priority = 128, -- this puts it right at the top
         })
       end
