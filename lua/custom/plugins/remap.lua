f8c630d55035cf7cfc46cfd69bd60c2870efbe75
j_oard_pfl <joard@pfl.com>
2024-11-11 11:31:27 -0700

    ctrl + s in insert mode

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -12,6 +12,7 @@ vim.keymap.set('i', '<C-c>', '<Esc>')
 vim.keymap.set('n', 'q', '<nop>')
 vim.keymap.set('i', '<C-m>', '<nop>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('i', '<C-s>', '<ESC>:w<CR>')
 vim.keymap.set('n', '<leader>e', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>E', vim.diagnostic.goto_prev)
 
