ad2e67d607fd2c359551787d2aa14cb7201508d2
j_oard_pfl <joard@pfl.com>
2024-12-20 16:39:40 -0700

    starting netcoredbg

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -62,7 +62,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        -- 'netcoredbg',
+        'netcoredbg',
       },
     }
 
@@ -77,19 +77,35 @@ return {
         icons = {
           pause = '⏸',
           play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          step_into = '⭏',
+          step_over = '↷',
+          step_out = '↑',
+          step_back = '⮢',
+          run_last = '↻',
+          terminate = '⮾',
+          disconnect = '⌀',
         },
+        enabled = true,
+        element = 'repl',
       },
     }
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    --[[
+    dap.configurations.cs = {
+      {
+        type = 'coreclr',
+        name = 'launch - netcoredbg',
+        request = 'launch',
+        console = 'integratedTerminal',
+        program = function()
+          return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end,
+      },
+    }
+    --]]
   end,
 }
