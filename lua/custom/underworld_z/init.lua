8e050ca5524ae10ce45486e1827aec1a8e820b27
Jeremiah D. Oard <64376668+MiahDrao97@users.noreply.github.com>
2024-12-22 18:21:50 -0700

    more rust highlighting

--- lua/custom/underworld_z/init.lua
+++ lua/custom/underworld_z/init.lua
@@ -258,6 +258,11 @@ M.descend_to_the_underworld = function()
           priority = 128, -- this puts it right at the top
         })
       end
+      if token.type == 'builtinAttribute' and vim.bo.filetype == 'rust' then
+        vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, 'underworld_z.struct', {
+          priority = 128, -- this puts it right at the top
+        })
+      end
       if token.type == 'lifetime' and vim.bo.filetype == 'rust' then
         vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, 'underworld_z.static', {
           priority = 128, -- this puts it right at the top
