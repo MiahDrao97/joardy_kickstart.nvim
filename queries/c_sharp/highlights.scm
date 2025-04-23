1ab7da9c1cea5f1b7e9b1056443b7e46979bea4c
Jeremiah D. Oard <JOard@pfl.com>
2025-04-23 10:59:42 -0600

    dynamic is a builtin type, not a kw

--- queries/c_sharp/highlights.scm
+++ queries/c_sharp/highlights.scm
@@ -1,8 +1,10 @@
 ;; inherits: c_sharp
 ;; extends
 
-((identifier) @keyword (#any-of? @keyword "nameof" "_" "dynamic"))
+((identifier) @keyword (#any-of? @keyword "nameof" "_"))
 ("base") @keyword
+
+((identifier) @type.builtin (#any-of? @type.builtin "dynamic"))
 
 ((comment) @comment
   (#lua-match? @comment "^/%*.*%*//$")
