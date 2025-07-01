7394036be6653c03a8c2f58a7687a1f69faaa289
Jeremiah D. Oard <64376668+MiahDrao97@users.noreply.github.com>
2025-06-30 21:00:07 -0600

    right amount of stiffness

--- lua/custom/plugins/smear_cursor.lua
+++ lua/custom/plugins/smear_cursor.lua
@@ -4,8 +4,8 @@ return {
   config = function()
     require('smear_cursor').setup {
       time_interval = 8,
-      stiffness = 0.9, -- 0.6      [0, 1]
-      trailing_stiffness = 0.7, -- 0.4      [0, 1]
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.84, -- 0.4      [0, 1]
       stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
       trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
       damping = 0.9, -- 0.65     [0, 1]
